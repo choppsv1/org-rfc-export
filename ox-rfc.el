f575dc017c6501d788bb5ab5fee3c9571b92188a
Christian E. Hopps <chopps@gmail.com>
2019-03-03 10:23:29 -0500

    Modify the description a bit.

--- ox-rfc.el
+++ ox-rfc.el
@@ -23,10 +23,12 @@
 
 ;;; Commentary:
 
-;; This library implements an RFC (xml2rfc) back-end for Org exporter, code was
-;; taken or modified from the `md' and `html' back-ends. It exports into XML as
-;; defined in RFC7991, and can then use `xml2rfc` to further convert to text.
-;; See Org manual for more information.
+;; This library implements an RFC (xml2rfc) export back-end for Org. It
+;; exports into XML as defined in RFC7749 (v2) and RFC7991 (v3), and can
+;; also use `xml2rfc` directly to further convert to HTML, PDF and TXT.
+;; See Org manual for more information. See README.org
+;; (https://github.com/choppsv1/org-rfc-export/blob/master/README.org)
+;; for basic documentation on use.
 
 ;;; Credits:
 
