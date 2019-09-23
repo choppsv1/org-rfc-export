81bdd60f18c9188b8c2acf539073e12fc3bbe05f
Christian E. Hopps <chopps@gmail.com>
2019-09-23 06:33:50 -0400

    Fix test to update revision date in comparison file.

--- ert-tests/ox-rfc-test.el
+++ ert-tests/ox-rfc-test.el
@@ -61,7 +61,8 @@
     (with-temp-file tmpvfile
       (insert (ox-rfc-load-tidy-xml-as-string verify-name))
       (dolist (x rubs)
-        (replace-regexp x "" nil (point-min) (point-max))))
+        (replace-regexp x "" nil (point-min) (point-max)))
+      (replace-regexp "revision 1900-01-01" (concat "revision " (format-time-string "%Y-%m-%d"))))
     (let ((diffout (shell-command-to-string (format "diff -c %s %s" tmpvfile tmpfile))))
       (if (and diffout (not (string= "" diffout)))
           (message "DIFF: %s" diffout))
